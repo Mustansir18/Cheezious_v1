41dc814ab51d7cc3f04d6045fd8e895d91ad91af
Mustansir Ali <mustansir133@gmail.com>
2025-12-25 17:59:54 +0000

    404

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,27 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 import AppLayout from './AppLayout';
+import { Poppins, PT_Sans } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: 'Cheezious',
   description: 'Order your favorite Cheezious meals online.',
 };
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-headline',
+  weight: ['400', '600', '700'],
+});
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+  weight: ['400', '700'],
+});
 
 export default function RootLayout({
   children,
@@ -14,12 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
+    <html lang="en" className={`${poppins.variable} ${ptSans.variable}`}>
       <body className="font-body antialiased">
         <AppLayout>
           {children}
