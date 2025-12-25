1dcddb2c0cfd4d8c57dc49f0b9762861e092fd33
Mustansir Ali <mustansir133@gmail.com>
2025-12-25 19:01:56 +0000

    Can you restore the app by deleting duplicates files and making the whol

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'httpshttps',
         hostname: 'cheezious.com',
         port: '',
         pathname: '/**',
