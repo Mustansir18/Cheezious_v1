2ab4a35c466ef3f665b982d3056ac7c38e141352
Mustansir Ali <mustansir133@gmail.com>
2026-01-03 09:50:19 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/app/admin/deals/page.tsx
+++ src/app/admin/deals/page.tsx
@@ -14,6 +14,7 @@ import {
   DialogFooter,
   DialogClose,
   DialogDescription,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
